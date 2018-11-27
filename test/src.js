8a99938580dd7d296fb60f6502da4cf836ae7d63
ChanWoo Kwon <arknell@iosys.co.kr>
2018-11-27 15:26:32 +0900

    init

--- test/src.js
+++ test/src.js
@@ -4,42 +4,9 @@
  * date : 2018-04-04
  */
 
-let analyzer = require("node-twitter-korean-text");
-let java = require('../node_modules/node-twitter-korean-text/node_modules/java');
-let rr = require('../src')(java);
+let rr = require('../src')(undefined);
 
-analyzer.tokenize("반가워요")
-.then((tokens) => {
-	return analyzer.tokensToJsonArray(tokens, false);
-})
-.then((result) => {
-	let co_occurrence = [];
-	for (let index in result) {
-		let token = result[index];
-
-		if (token.koreanPos == "Noun" || token.koreanPos == "Adjective" || token.koreanPos == "ProperNoun") {
-			if (co_occurrence.hasOwnProperty(token.text)) {
-				co_occurrence[token.text].count++;
-			} else {
-				co_occurrence[token.text] = {
-					count: 1
-				}
-			}
-		}
-	}
-
-	let send_message = "";
-
-	for (let text in co_occurrence) {
-		send_message += text + ":" + (co_occurrence[text].count * 10) + " ";
-	}
-	console.log(result);
-	
-	rr.read('z://Earlgae2014/01/78/017801/quizdata/2018/03/30/DF0B2F3A7BAA4B5BBA2A636E19A9D346/_205-Economist.pdf')
-	.then((res) => {
-		console.log(res);
-	});
-})
-.catch((err) => {
-	console.log(err);
+rr.read('z://Earlgae2014/01/78/017801/quizdata/2018/03/30/DF0B2F3A7BAA4B5BBA2A636E19A9D346/_205-Economist.pdf')
+.then((res) => {
+	console.log(res);
 });
